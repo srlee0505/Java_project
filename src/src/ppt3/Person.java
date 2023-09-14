44a10639040ae5a188c3fb1175e8b4ab5da94956
srlee <sr120505@naver.com>
2023-09-14 10:36:47 +0900

    ?

--- src/src/ppt3/Person.java
+++ src/src/ppt3/Person.java
@@ -13,11 +13,11 @@ public class Person {
 	private String rrn; 
 	
 	// default constructor() 
-	// 기본생성자가 부모클래스에 없을 경우에는, 자식 클래스에서 부모의 생성자를 무조건 명시하여 호출해야 한다.
-	// 기본생성자가 부모클래스에 있는 경우에는, 자식 클래스에서 부모의 생성자 호출을 생략해도, 자동으로 기본 생성자를 호출해준다.
-	//public Person() {
-	//	System.out.println("Person의 기본 생성자");
-	//}
+	// 부모클래스에 기본생성자가 없을 경우에는, 자식 클래스에서 부모의 생성자를 무조건 명시하여 호출해야 한다.
+	// 부모클래스에 기본생성자가 있는 경우에는, 자식 클래스에서 부모의 생성자 호출을 생략해도, 자동으로 기본 생성자를 호출해준다.
+	public Person() {
+		System.out.println("Person의 기본 생성자");
+	}
 	
 	// constructor(name, age, rrn)
 	public Person(String name, int age, String rrn) {
@@ -26,10 +26,7 @@ public class Person {
 		this.age = age;
 		this.rrn = rrn;
 	}
-	
-	/**
-	 * 
-	 */
+
 	public void greeting() {
 		System.out.println("안녕하세요. 저는 " + name + "입니다." );
 	}
